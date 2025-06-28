c33042e7eacd61fd7e30fddb1b238b3cbca93d1f
unknown <haidi_zakaria@yahoo.com>
2025-06-28 16:28:54 +0300

    Add VPC, firewall rule, and VM with Apache setup

--- main.tf
+++ main.tf
@@ -1,17 +1,36 @@
-<<<<<<< HEAD
-# main.tf
-=======
->>>>>>> d93acc4 (Rebuild GCP Terraform lab)
 provider "google" {
   project = var.project_id
   region  = var.region
   zone    = var.zone
 }
 
+# VPC Network
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-vpc"
+  auto_create_subnetworks = true
+}
+
+# Firewall Rule to Allow HTTP (port 80)
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
+
+# VM Instance with Apache Startup Script
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
-  machine_type = "f1-micro"
+  machine_type = "e2-micro"
   zone         = var.zone
+
+  tags = ["http-server"]
 
   boot_disk {
     initialize_params {
@@ -20,11 +39,19 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    network       = "default"
-    access_config {}
+    network = google_compute_network.vpc_network.name
+
+    access_config {
+      # Creates external IP
+    }
   }
-<<<<<<< HEAD
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    apt-get update
+    apt-get install -y apache2
+    systemctl start apache2
+    systemctl enable apache2
+  EOT
 }
-=======
-}
->>>>>>> d93acc4 (Rebuild GCP Terraform lab)
+
--- variables.tf
+++ variables.tf
@@ -3,22 +3,27 @@
 =======
 >>>>>>> d93acc4 (Rebuild GCP Terraform lab)
 variable "project_id" {
-  description = "The GCP project ID"
+  description = "GCP Project ID"
   type        = string
+  default     = "persuasive-byte-464119-k7"
 }
 
 variable "region" {
-  description = "The GCP region"
+  description = "GCP Region"
   type        = string
   default     = "europe-west1"
 }
 
 variable "zone" {
-  description = "The GCP zone"
+  description = "GCP Zone"
   type        = string
   default     = "europe-west1-b"
+<<<<<<< HEAD
 <<<<<<< HEAD
 }
 =======
 }
 >>>>>>> d93acc4 (Rebuild GCP Terraform lab)
+=======
+}
+>>>>>>> 6316907 (Complete GCP Terraform setup with VPC, firewall, VM)
